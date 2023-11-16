f7ce3ad84c719f6dd9fba7504040192ec4a7dba2
esatpiskin <wraithrayy@protonmail.com>
2023-11-16 16:59:59 +0300

    homescreen added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,29 +1,55 @@
-import {React, useEffect} from 'react';
-import { useNavigate } from "react-router-dom";
-import { getCookie, deleteCookie } from '../../cookie-functions';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './homepage.css'; // Ensure the CSS file is imported
 
-const MainScreen = () => {
-
+const HomePage = () => {
     const navigate = useNavigate();
-        useEffect(() => {
-            if(!getCookie('sopp-auth')){    
-                navigate("/login");
-            }
-        },[])
+    const [showMenu, setShowMenu] = useState(false);
 
-    const handleLogout = () => {            
-        deleteCookie('sopp-auth')
-        navigate("/login");
-        window.location.reload();
-      };
+    const handleCreatePayment = () => {
+        navigate('/home/create-payment');
+    };
 
+    const handlePaymentHistory = () => {
+        navigate('/home/payment-history');
+    };
 
-  return (
-    <div className="main-screen">
-      <h1>Main Screen</h1>      
-      <button onClick={handleLogout}>Log Out</button>
-    </div>
-  );
+    const handleWithdrawMoney = () => {
+        console.log('Withdraw Money Clicked');
+    };
+
+    const handleMenuToggle = () => {
+      setShowMenu(!showMenu);
+      console.log('Menu Toggled', !showMenu); // This will log the new state
+    };
+
+    const handleProfile = () => {
+        console.log('Profile Clicked'); // Placeholder for profile functionality
+    };
+
+    const handleExit = () => {
+        console.log('Exit Clicked');
+    };
+
+    return (
+        <div className="main-screen">
+            <h1>Merchant Dashboard</h1>
+            <div className="menu">
+                <button onClick={handleMenuToggle}>Menu</button>
+                {showMenu && (
+                    <div className="dropdown-menu">
+                        <button onClick={handleProfile}>My Profile</button>
+                    </div>
+                )}
+            </div>
+            <div className="exit-button">
+                <button onClick={handleExit}>Exit</button>
+            </div>
+            <button onClick={handleCreatePayment}>Receive Payment</button>
+            <button onClick={handlePaymentHistory}>Payment History</button>
+            <button onClick={handleWithdrawMoney}>Withdraw Money</button>
+        </div>
+    );
 };
 
-export default MainScreen;
+export default HomePage;
