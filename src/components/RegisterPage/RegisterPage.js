96e44eb5910e3827af02353936a26b1bf95808ef
omergguler <oguler19@ku.edu.tr>
2023-12-12 15:47:12 +0300

    Update register screen

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -6,6 +6,9 @@ import "./RegisterPage.scss";
 function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
 
 
   const handleRegister = async (e) => {
@@ -52,6 +55,26 @@ function RegisterPage() {
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="input-container">
+                  <input
+                    class="form-control form-control-lg"
+                    type="text"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="input-container">
+                  <input
+                    class="form-control form-control-lg"
+                    type="text"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
                     required
                   />
                 </div>
